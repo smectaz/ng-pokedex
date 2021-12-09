385739575f95b34ea2577919d291549685427d33
smectaz <90684643+smectaz@users.noreply.github.com>
2021-12-09 12:15:06 +0100

    test formmodule

--- src/app/components/pokemon-item/pokemon-item.component.ts
+++ src/app/components/pokemon-item/pokemon-item.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonItemComponent implements OnInit {
 nbCaught = 0;
-  constructor() { }
+buttonAddClicked = false;
+buttonRemoveClicked = false;
+pokemonName = '';
+zeroPokemon = '';
+
+constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  pokemonNameChanged($event: Event){
+    console.log($event);
+    const inputElement = $event.target as HTMLInputElement;
+    this.pokemonName = inputElement.value;
+  }
+
+  addPokemonButton($event:Event) {
+  console.log($event);
+  this.nbCaught ++;
+  this.buttonAddClicked = !this.buttonAddClicked;
+}
+
+removePokemonButton($event:Event) {
+  console.log($event)
+
+  if (this.nbCaught==0){
+    this.zeroPokemon = "Tu n'as plus de pokémon";
+  } else {
+  this.nbCaught --;
+
+  this.buttonRemoveClicked = !this.buttonRemoveClicked;
+  }
+}
 }
